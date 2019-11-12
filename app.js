22a17dcba35c843f59f0c91e597f499a47800644
P4T4PON <r4ptor1@onet.pl>
2019-11-12 19:53:57 +0100

    commited

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 window.addEventListener("load", getCompanies);
 
+//catch div to display things
 let output = document.getElementById("output");
 
+//fetch json file
 function getCompanies() {
   fetch("db.json")
-    .then(function(response) {
+    .then(response => {
       return response.json();
     })
-    .then(function(data) {
+    .then(data => {
+      // sort companies ascending depends on number of users
       let companies = data.companies.sort((x, y) =>
         data.users.filter(obj => {
           return obj.uris.company == x.uri;
@@ -25,29 +28,38 @@ function getCompanies() {
           ? -1
           : 0
       );
+
       data.companies = companies;
+      // for number of companies create div companies with id and company name
       for (let i = 0; i < data.companies.length; i++) {
         output.innerHTML += `<div class="center"><div class="companies" id=${i}><div class="companyName">${data.companies[i].name}</div></div><div class = "expand btn">+</div></div></br>`;
       }
-      document.querySelectorAll(".btn").forEach(function(e) {
-        etc = e.previousSibling;
+      // catch every button
+      document.querySelectorAll(".btn").forEach(e => {
+        pervious = e.previousSibling;
 
-        data.users.filter(function(ur) {
-          if (ur.uris.company === data.companies[etc.id].uri) {
-            etc.innerHTML += `<div class="display-none">Name: ${ur.name} : </br>Email: ${ur.email}</br></div>`;
+        //filter through users and give them class display-none by default
+        data.users.filter(element => {
+          if (element.uris.company === data.companies[pervious.id].uri) {
+            pervious.innerHTML += `<div class="display-none">Name: ${element.name} : </br>Email: ${element.email}</br></div>`;
           }
         });
 
-        e.addEventListener("click", function(el) {
+        //if u click '+' show users if '-' hide them
+        e.addEventListener("click", el => {
           for (
+            // start from 1 becouse 0 element is company Name
             let i = 1;
             i < el.target.parentElement.children[0].children.length;
             i++
           ) {
+            // if u click '+' show users
             if (el.target.innerHTML === "+") {
               el.target.parentElement.children[0].children[i].className =
                 "users";
-            } else if (el.target.innerHTML === "-") {
+            }
+            // if u click '-' hide users
+            else if (el.target.innerHTML === "-") {
               el.target.parentElement.children[0].children[i].className =
                 "display-none";
             }
